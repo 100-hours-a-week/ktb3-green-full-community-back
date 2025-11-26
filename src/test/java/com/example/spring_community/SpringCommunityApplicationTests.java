01a2d958cb0a8b6bf03b583f865d1798c02f5346
jiyeonyooo <ji04230423@gmail.com>
2025-11-26 21:01:44 +0900

    chore: spring boot 기본 테스트 코드

---
+++ src/test/java/com/example/spring_community/SpringCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
